4129b6f43773cf6806387ff747c6ab4d52b3f1d9
Andrea-Mazza <mazzaandrea45@gmail.com>
2022-09-11 17:17:10 +0200

    workin on project section responsive settings

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     // TABLE OF CONTENT
     // 1. CAROUSEL ANIMATION
+    // 2. PROJECT SECTION STRUCTURE'S CHANGES
 
     // 1. CAROUSEL ANIMATION
 
     const carouselContainer = document.getElementById('carouselContainer');
+    const carousel = document.getElementById('carousel');
     const carouselItem = Array.from(document.getElementsByClassName('carousel-item'));
 
     const itemMediumAnimation = [
@@ -23,34 +25,81 @@ document.addEventListener('DOMContentLoaded', function () {
         fill: 'both'
     };
 
-    carouselItem.forEach(item => {
-        if (item.offsetLeft === 0) {
-            item.style.width = '350px';
-            item.style.height = '350px';
-        } else if (item.offsetLeft === 370) {
-            item.style.width = '300px';
-            item.style.height = '300px';
+    if (window.matchMedia('(min-width: 1243px)').matches) {
+        carouselItem.forEach(item => {
+            if (item.offsetLeft === 0) {
+                item.style.width = '350px';
+                item.style.height = '350px';
+            } else if (item.offsetLeft === 370) {
+                item.style.width = '300px';
+                item.style.height = '300px';
 
-            item.addEventListener('mouseenter', function () {
-                item.animate(itemMediumAnimation, itemTiming);
-            });
+                item.addEventListener('mouseenter', function () {
+                    item.animate(itemMediumAnimation, itemTiming);
+                });
 
-            item.addEventListener('mouseleave', function () {
-                item.animate(itemMediumAnimation, itemTiming).reverse();
-            });
-        } else {
-            item.style.width = '250px';
-            item.style.height = '250px';
+                item.addEventListener('mouseleave', function () {
+                    item.animate(itemMediumAnimation, itemTiming).reverse();
+                });
+            } else {
+                item.style.width = '250px';
+                item.style.height = '250px';
 
-            item.addEventListener('mouseenter', function () {
-                item.animate(itemSmallAnimation, itemTiming);
-            });
+                item.addEventListener('mouseenter', function () {
+                    item.animate(itemSmallAnimation, itemTiming);
+                });
 
-            item.addEventListener('mouseleave', function () {
-                item.animate(itemSmallAnimation, itemTiming).reverse();
-            });
-        }
-    });
-
+                item.addEventListener('mouseleave', function () {
+                    item.animate(itemSmallAnimation, itemTiming).reverse();
+                });
+            }
+        });
+    }
     // CAROUSEL ANIMATION END
+
+
+
+    // 2. PROJECT SECTION STRUCTURE'S CHANGES
+
+    // if (window.matchMedia('(max-width: 1242px)').matches) {
+    //     if (carousel.hasChildNodes) {
+    //         carouselItem.forEach(element => element.remove());
+    //     }
+    //     const singleElementDiv = document.createElement('div');
+    //     singleElementDiv.classList.add('carousel-item');
+    //     singleElementDiv.classList.add('item-1');
+    //     singleElementDiv.style.width = '350px';
+    //     singleElementDiv.style.height = '350px';
+
+    //     carousel.appendChild(singleElementDiv);
+
+    //     const doubleElementDiv = document.createElement('div');
+    //     doubleElementDiv.classList.add('double-item');
+
+    //     carousel.appendChild(doubleElementDiv);
+
+    //     const topDiv = document.createElement('div');
+    //     topDiv.classList.add('top-div');
+    //     topDiv.classList.add('item-2');
+    //     topDiv.classList.add('carousel-item');
+    //     topDiv.style.width = '250px';
+    //     topDiv.style.height = '250px';
+
+    //     doubleElementDiv.appendChild(topDiv);
+
+    //     const bottomDiv = document.createElement('div');
+    //     bottomDiv.classList.add('bottom-div');
+    //     bottomDiv.classList.add('item-3');
+    //     bottomDiv.classList.add('carousel-item');
+    //     bottomDiv.style.width = '250px';
+    //     bottomDiv.style.height = '250px';
+
+    //     doubleElementDiv.appendChild(bottomDiv);
+    // } else if (window.matchMedia('(max-width: 883px)').matches) {
+    //     if (carousel.hasChildNodes) {
+    //         carouselItem.forEach(element => element.remove());
+    //     }
+    // }
+
+    // PROJECT SECTION STRUCTURE'S CHANGES END
 });
