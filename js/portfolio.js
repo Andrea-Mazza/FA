faa2fd12845ae2ee9272f5fb4ed3cb5016f04ef2
Andrea-Mazza <mazzaandrea45@gmail.com>
2022-10-15 09:02:34 +0200

    add final version of portfolio section and page

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     const previewCard = Array.from(document.getElementsByClassName('preview-card'));
+    const icon = ['./assets/icons/html-5.svg',
+        './assets/icons/css3.svg',
+        './assets/icons/sass.svg',
+        './assets/icons/javascript.svg'];
+    const bootstrapIcon = './assets/icons/bootstrap.svg';
 
     for (let i = 0; i < previewCard.length; i++) {
         const card = previewCard[i];
@@ -10,7 +15,29 @@ document.addEventListener('DOMContentLoaded', function () {
         const p = document.createElement('p');
         p.innerHTML = `Costruito con:`;
 
+        const iconContainer = document.createElement('div');
+        iconContainer.classList.add('tech-icon-container');
+
+        for (let j = 0; j < icon.length; j++) {
+            const img = document.createElement('img');
+
+            img.src = icon[j];
+            img.classList.add('img-fluid');
+
+            iconContainer.appendChild(img);
+        }
+
+        //This condition must be increased by 1 each time you add new templates into arrays in preview.js
+        if (i < 1) {
+            const bootstrap = document.createElement('img');
+            bootstrap.src = `${bootstrapIcon}`;
+
+            bootstrap.classList.add('img-fluid');
+            iconContainer.appendChild(bootstrap);
+        }
+
         description.appendChild(p);
+        description.appendChild(iconContainer);
         card.appendChild(description);
     }
 });
--- js/preview.js
+++ js/preview.js
@@ -5,18 +5,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const templateImgPath = `./assets/template_preview/`;
 
 
-    const title = ['arsha', 'digimedia', 'leadership event', 'first portfolio'];
-    const imgSrc = [`${templateImgPath}arsha.svg`, `${templateImgPath}digimedia.svg`, `${templateImgPath}leadershipEvent.svg`, `${templateImgPath}firstPortfolio.svg`];
-    const href = [`${previewPath}arsha.html`, `${previewPath}digimedia.html`, `${previewPath}leadershipEvent.html`, `${previewPath}firstPortfolio.html`];
+    const title = ['first portfolio', 'leadership event', 'digimedia', 'arsha'];
+    const imgSrc = [`${templateImgPath}firstPortfolio.svg`, `${templateImgPath}leadershipEvent.svg`, `${templateImgPath}digimedia.svg`, `${templateImgPath}arsha.svg`];
+    const href = [`${previewPath}firstPortfolio.html`, `${previewPath}leadershipEvent.html`, `${previewPath}digimedia.html`, `${previewPath}arsha.html`];
+
+    //IMPORTANT: Once you've added new templates into these arrays, you must increase by 1 the variable 'i'
+    //in 'portfolio.js'
 
 
 
-    for (let i = 0; i < title.length; i++) {
+    for (let i = title.length - 1; i >= 0; i--) {
         const col = document.createElement('div');
         col.classList.add('preview-card');
         col.classList.add('col-12');
         col.classList.add('col-lg-3');
         col.classList.add('col-md-6');
+        col.classList.add('col-sm-6');
         col.classList.add('d-flex');
         col.classList.add('justify-content-center');
 
@@ -26,11 +30,16 @@ document.addEventListener('DOMContentLoaded', function () {
         const img = document.createElement('img');
         img.src = `${imgSrc[i]}`;
         img.classList.add('img-fluid');
+        img.classList.add('preview-img');
 
         a.appendChild(img);
         col.appendChild(a);
         previewContainer.appendChild(col);
     }
 
+
+    const totalTemplate = document.getElementById('totalTemplates');
+
+    totalTemplate.innerHTML = title.length;
 
 });
